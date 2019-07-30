a1a50ad5bd4f0744610684bd3ec588e6376e4e07
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2019-07-30 18:29:40 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sherlock
+{
+    class Program
+    {
+        static Sherlock sherlock = new Sherlock();
+
+        static void Main(string[] args)
+        {
+            sherlock.start(args[0]);
+        }
+    }
+}
---
+++ Sherlock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace sherlock
+{
+    class Sherlock
+    {
+        /*
+         * 1st line = Site name
+         * 2nd line = Site string
+         * 3rd line = Site contains
+         */
+        public void start(string Username)
+        {
+            string sitesDir = Directory.GetCurrentDirectory() + "\\sites\\";
+            string[] files = Directory.GetFiles(sitesDir);
+
+            int i = 0;
+            while (i < files.Length)
+            {
+                string currentFile = files[i];
+                string[] currentLines = File.ReadAllLines(currentFile);
+
+                string siteName = currentLines[0];
+                string siteString = currentLines[1];
+                string siteContains = currentLines[2];
+
+                WebClient wc = new WebClient();
+                    
+                try
+                {
+                    string source = wc.DownloadString(siteString + Username);
+                   
+                    if (source.Contains(siteContains))
+                    {
+                        Console.Write("- " + siteName + "[");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("+");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("] - ");
+                        Console.WriteLine(siteString + Username);
+                    }
+
+                    else
+                    {
+                        Console.Write("- " + siteName + " [");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("-");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("] - ");
+                        Console.WriteLine("User Not Found");
+                    }
+                }
+                catch
+                {
+                    Console.Write("- " + siteName + " [");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("-");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("] - ");
+                    Console.WriteLine("User Not Found");
+                }
+
+                i++;
+            }
+        }
+    }
+}
